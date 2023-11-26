d60b0a32a7fba3c5086a9042765c2a0a058e9f5a
zin-wai-aung <zinwaiaung60@gmail.com>
2023-11-26 10:59:06 +0630

    project init

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>Vue Bloggy System</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+
+<style>
+.home{
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
